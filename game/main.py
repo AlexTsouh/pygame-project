f8e6cf8872b912feb6025140ccc3e674ded4fd82
AlexTsouh <alextheprogr@gmail.com>
2023-10-25 16:44:11 +0300

    sdfregertg

--- game/main.py
+++ game/main.py
@@ -21,7 +21,7 @@ def main():
     platforms = []
     particles = []
 
-    f = open("game/lvl.json")
+    f = open("lvl.json")
     data= json.load(f)
     for i in data['lvl']:
         platforms.append(i)
