c09e1e942b1ce7af84bb05c65b0b467bb2763d90
AlexTsouh <alextheprogr@gmail.com>
2023-10-26 22:40:09 +0300

    editor

--- editor/edit.py
+++ editor/edit.py
@@ -3,27 +3,24 @@ from pygame.locals import *
 from sys import exit
 pygame.init()
 import json
-import time
 
 def main():
+    font = pygame.font.Font('freesansbold.ttf', 32)
     screen_w = 1920
     screen_h = 1025
-    
     player_x_vel = 0
     player_y_vel = 0
     player_jump = False
-
+    cursor_color = "red"
     screen = pygame.display.set_mode((screen_w,screen_h))
-
     player = pygame.Rect((screen_w/2,screen_h/2,50,50))
     clock = pygame.time.Clock()
-    
     platforms = []
-
     f = open("game\lvl.json")
     data= json.load(f)
     for i in data['lvl']:
         platforms.append(i)
+    pygame.mouse.set_visible(0)
 
     def collide():
         c = False
@@ -37,6 +34,7 @@ def main():
     while run:
         clock.tick(165)
         screen.fill((0,0,0))
+        mx,my = pygame.mouse.get_pos()
 
         #movement
         key = pygame.key.get_pressed()
@@ -70,14 +68,22 @@ def main():
                 player_y_vel += 0.15
         player.move_ip(player_x_vel, player_y_vel)
 
-        #draw
+        #draw 
+        if pygame.mouse.get_pressed() == True:
+            cursor_color = "cyan"
+        else:
+            cursor_color = "red"
+        pygame.draw.line(screen, "green", [0, my], [screen_w, my], 1)
+        pygame.draw.line(screen, "green", [mx, 0], [mx, screen_h], 1)
         pygame.draw.rect(screen, (255,255,255), player)
         for e in platforms:
             pygame.draw.rect(screen, "white", pygame.Rect((int(e['x']), int(e['y']), int(e['w']), int(e['h']))))
-            
+        pygame.draw.line(screen, cursor_color, [mx-10, my], [mx+10, my], 2)
+        pygame.draw.line(screen, cursor_color, [mx, my-10], [mx, my+10], 2)
+
         #quit
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == QUIT:
                 run = False
             
         pygame.display.update()
